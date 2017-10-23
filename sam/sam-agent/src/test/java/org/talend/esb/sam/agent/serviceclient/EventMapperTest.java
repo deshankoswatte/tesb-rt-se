aa6feef2f012956418d1b922fc9e2a0d7f3e9e55
apoltavtsev@gmail.com <apoltavtsev@gmail.com>
2017-10-23 22:52:34 +0300

    TESB-20332 Set of JUnit tests for SAM Agent is extended

--- sam/sam-agent/src/test/java/org/talend/esb/sam/agent/serviceclient/EventMapperTest.java
+++ sam/sam-agent/src/test/java/org/talend/esb/sam/agent/serviceclient/EventMapperTest.java
@@ -29,10 +29,14 @@ import javax.activation.DataHandler;
 import junit.framework.Assert;
 
 import org.apache.cxf.helpers.IOUtils;
+import org.easymock.EasyMock;
 import org.junit.Test;
 import org.talend.esb.sam._2011._03.common.EventType;
 import org.talend.esb.sam.common.event.Event;
+import org.talend.esb.sam.common.event.MessageInfo;
+import org.talend.esb.sam.common.event.Originator;
 
+@SuppressWarnings("deprecation")
 public class EventMapperTest {
 	
 	@Test
@@ -46,8 +50,54 @@ public class EventMapperTest {
 		// TODO test the other properties
 	}
 
-	private String getContent(DataHandler dh) throws IOException,
-			UnsupportedEncodingException {
+	@Test
+	public void testEventMapper2() throws IOException {
+		Event event = new Event();
+		event.setContent("testContent");
+
+		MessageInfo messageInfo = EasyMock.createMock(MessageInfo.class);
+		EasyMock.expect(messageInfo.getMessageId()).andReturn("MessageId").anyTimes();
+		EasyMock.expect(messageInfo.getFlowId()).andReturn("FlowId").anyTimes();
+		EasyMock.expect(messageInfo.getPortType()).andReturn("PortType").anyTimes();
+		EasyMock.expect(messageInfo.getOperationName()).andReturn("OperationName").anyTimes();
+		EasyMock.expect(messageInfo.getTransportType()).andReturn("TransportType").anyTimes();
+		EasyMock.replay(messageInfo);
+		event.setMessageInfo(messageInfo);
+
+		Originator originator = EasyMock.createMock(Originator.class);
+		EasyMock.expect(originator.getProcessId()).andReturn("ProcessId").anyTimes();
+		EasyMock.expect(originator.getIp()).andReturn("Ip").anyTimes();
+		EasyMock.expect(originator.getHostname()).andReturn("Hostname").anyTimes();
+		EasyMock.expect(originator.getCustomId()).andReturn("CustomId").anyTimes();
+		EasyMock.expect(originator.getPrincipal()).andReturn("Principal").anyTimes();
+		EasyMock.replay(originator);
+		event.setOriginator(originator);
+
+		event.getCustomInfo().put("testKey", "testValue");
+
+		EventType eventOut = EventMapper.map(event);
+		DataHandler dh = eventOut.getContent();
+		String outContent = getContent(dh);
+		Assert.assertEquals(event.getContent(), outContent);
+
+		Assert.assertEquals(eventOut.getOriginator().getProcessId(), event.getOriginator().getProcessId());
+		Assert.assertEquals(eventOut.getOriginator().getIp(), event.getOriginator().getIp());
+		Assert.assertEquals(eventOut.getOriginator().getHostname(), event.getOriginator().getHostname());
+		Assert.assertEquals(eventOut.getOriginator().getCustomId(), event.getOriginator().getCustomId());
+		Assert.assertEquals(eventOut.getOriginator().getPrincipal(), event.getOriginator().getPrincipal());
+
+		Assert.assertEquals(eventOut.getMessageInfo().getMessageId(), event.getMessageInfo().getMessageId());
+		Assert.assertEquals(eventOut.getMessageInfo().getFlowId(), event.getMessageInfo().getFlowId());
+		Assert.assertSame(eventOut.getMessageInfo().getPorttype().getLocalPart(), event.getMessageInfo().getPortType());
+		Assert.assertEquals(eventOut.getMessageInfo().getOperationName(), event.getMessageInfo().getOperationName());
+		Assert.assertEquals(eventOut.getMessageInfo().getTransport(), event.getMessageInfo().getTransportType());
+
+		Assert.assertEquals(eventOut.getCustomInfo().getItem().get(0).getValue(), "testValue");
+		Assert.assertEquals(eventOut.getCustomInfo().getItem().get(0).getKey(), "testKey");
+
+	}
+
+	private String getContent(DataHandler dh) throws IOException, UnsupportedEncodingException {
 		InputStream is = dh.getInputStream();
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		IOUtils.copy(is, bos);
