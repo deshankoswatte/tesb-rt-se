1944bfa5b874d1429811979a40b56cde25171717
XiLai Dai <xldai@talend.com>
2019-03-14 15:43:42 +0800

    TESB-24618 solved the jaxb/activation class loading problem

--- sam/sam-common/src/main/java/org/talend/esb/sam/common/event/MessageInfo.java
+++ sam/sam-common/src/main/java/org/talend/esb/sam/common/event/MessageInfo.java
@@ -21,8 +21,8 @@ package org.talend.esb.sam.common.event;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 //@Embeddable
 /**
--- sam/sam-service-soap/src/test/java/org/talend/esb/sam/soap/service/EventRepositoryTest.java
+++ sam/sam-service-soap/src/test/java/org/talend/esb/sam/soap/service/EventRepositoryTest.java
@@ -24,7 +24,7 @@ import java.util.GregorianCalendar;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
